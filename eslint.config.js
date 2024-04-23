954d76bd9a5de25bc8feb1f66a078845116d25b4
kovaldos <ya.kovalev@ya-kovalev.ru>
2024-04-23 21:46:50 +0300

    init project

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import pluginVue from 'eslint-plugin-vue'
+export default [
+    // add more generic rulesets here, such as:
+    // js.configs.recommended,
+    ...pluginVue.configs['flat/recommended'],
+    // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
+    {
+        rules: {
+            // override/add rules settings here, such as:
+            'vue/no-unused-vars': 'error',
+            "vue/html-indent": ["error", "tab", {
+                "attribute": 1,
+                "baseIndent": 2,
+                "closeBracket": 0,
+                "alignAttributesVertically": true,
+                "ignores": []
+            }]
+        }
+    }
+]
