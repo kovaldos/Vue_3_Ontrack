7e0fc5de239611182d1f902d5facbab7b90c57bf
kovaldos <ya.kovalev@ya-kovalev.ru>
2024-05-28 20:07:41 +0300

    Add url hash normalizer

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -6,14 +6,25 @@ import {
 } from '@heroicons/vue/24/outline/index.js'
 import NavItem from './NavItem.vue'
 import {ref} from "vue";
+import {PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS} from "../constans";
 
 const navItems = {
-    timeline: ClockIcon,
-    activities: ListBulletIcon,
-    progress: ChartBarIcon,
+    [PAGE_TIMELINE]: ClockIcon,
+    [PAGE_ACTIVITIES]: ListBulletIcon,
+    [PAGE_PROGRESS]: ChartBarIcon,
 }
 
-const currentPage = ref('timeline');
+const currentPage = ref(normalizeHash());
+
+function normalizeHash() {
+  const hash = window.location.hash.slice(1)
+  if(Object.keys(navItems).includes(hash)) {
+    return hash;
+  }
+  window.location.hash = PAGE_TIMELINE;
+  return PAGE_TIMELINE;
+}
+
 </script>
 
 <template>
