817808e560f9252f5a77fc931af90e458abfac05
kovaldos <ya.kovalev@ya-kovalev.ru>
2024-06-20 21:59:04 +0300

    Add selection layout

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,12 +1,20 @@
 <script setup>
+import { XMarkIcon } from '@heroicons/vue/24/outline'
+
 // eslint-disable-next-line vue/require-prop-types
-const props = defineProps(['timelineItem'])
+const props = defineProps(['timelineItem']);
 
 const hourLinkClasses = [
     'absolute -top-4 left-1/2 -translate-x-1/2 rounded px-2 font-mono text-lg',
     props.timelineItem.hour === new Date().getHours()
         ? 'bg-purple-900 font-black text-white'
         : 'bg-gray-100 text-gray-500',
+]
+
+const options = [
+  {value: 1,label: 'Coding'},
+  {value: 2,label: 'Reading'},
+  {value: 3,label: 'Training'},
 ]
 </script>
 
@@ -18,5 +26,30 @@ const hourLinkClasses = [
       href="#"
       :class="hourLinkClasses"
     > {{ timelineItem.hour }}:00 </a>
+    <div class="flex gap-2">
+      <button class="rounded bg-gray-100 p-3 enabled:hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50">
+        <XMarkIcon class="h-8" />
+      </button>
+      <select
+        id=""
+        name=""
+        class="w-full truncate rounded bg-gray-100 px-2 py-1 text-2xl"
+      >
+        <option
+          selected
+          disabled
+          value=""
+        >
+          Rest
+        </option>
+        <option
+          v-for="{ value, label } in options"
+          :key="value"
+          :value="value"
+        >
+          {{ label }}
+        </option>
+      </select>
+    </div>
   </li>
 </template>
