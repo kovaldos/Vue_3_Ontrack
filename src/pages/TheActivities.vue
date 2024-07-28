082208b3c7426a24b1686d97437a0168a6863abc
kovaldos <ya.kovalev@ya-kovalev.ru>
2024-07-28 10:58:19 +0300

    added  Activities page layout

--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,11 +1,52 @@
 <script setup>
+import { TrashIcon } from '@heroicons/vue/24/outline/index'
+import BaseButton from '../components/BaseButton.vue'
+import BaseSelect from '../components/BaseSelect.vue'
 
+const activities = ['coding', 'reading', 'training']
+
+const periodSelectOptions = [
+    {
+        value: 15,
+        label: '0:15',
+    },
+    {
+        value: 30,
+        label: '0:30',
+    },
+    {
+        value: 45,
+        label: '0:45',
+    },
+]
 </script>
 
 <template>
-    <h1>The Activities</h1>
+    <div>
+        <ul class="divide-y">
+            <li
+                v-for="activity in activities"
+                :key="activity"
+                class="flex flex-col gap-2 p-4"
+            >
+                <div class="flex items-center gap-2">
+                    <BaseButton>
+                        <TrashIcon class="h-8" />
+                    </BaseButton>
+                    <span class="truncate text-xl first-letter:uppercase">{{
+                        activity
+                    }}</span>
+                </div>
+                <div>
+                    <BaseSelect
+                        placeholder="h:mm"
+                        :options="periodSelectOptions"
+                        class="font-mono"
+                    />
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
