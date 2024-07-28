a9998f6b5317d36ac7d3657012f92648ac337382
kovaldos <ya.kovalev@ya-kovalev.ru>
2024-07-28 11:11:13 +0300

    added ActivityItem component

---
+++ src/components/ActivityItem.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { TrashIcon } from '@heroicons/vue/24/outline/index'
+import BaseButton from './BaseButton.vue'
+import BaseSelect from './BaseSelect.vue'
+import {ref} from "vue";
+
+defineProps({
+    activity: {
+        type: String,
+        required: true,
+    }
+})
+
+const periodSelectOptions = [
+    {
+        value: 15,
+        label: '0:15',
+    },
+    {
+        value: 30,
+        label: '0:30',
+    },
+    {
+        value: 45,
+        label: '0:45',
+    },
+]
+
+const secondsToComplete = ref(15);
+</script>
+
+<template>
+    <li class="flex flex-col gap-2 p-4">
+        <div class="flex items-center gap-2">
+            <BaseButton>
+                <TrashIcon class="h-8" />
+            </BaseButton>
+            <span class="truncate text-xl first-letter:uppercase">
+                {{ activity }}
+            </span>
+        </div>
+        <div>
+            <BaseSelect
+                placeholder="h:mm"
+                :options="periodSelectOptions"
+                :selected="secondsToComplete"
+                class="font-mono"
+                @select="secondsToComplete = $event"
+            />
+        </div>
+    </li>
+</template>
+
+<style scoped></style>
