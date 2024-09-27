05fddaa2a3c8d671775239be3917807888192250
kovaldos <ya.kovalev@ya-kovalev.ru>
2024-09-27 10:48:53 +0300

    added activity adding form

--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,13 +1,15 @@
 <script setup>
 import ActivityItem from '../components/ActivityItem.vue'
-import {isActivityValid, validateActivities} from '../validators';
+import { isActivityValid, validateActivities } from '../validators'
+import { PlusIcon } from '@heroicons/vue/24/outline/index'
+import BaseButton from '../components/BaseButton.vue'
 
 defineProps({
     activities: {
         type: Array,
         required: true,
         validator: validateActivities,
-    }
+    },
 })
 
 const emit = defineEmits({
@@ -25,6 +27,16 @@ const emit = defineEmits({
                 @delete="emit('deleteActivity', activity)"
             />
         </ul>
+        <form class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
+            <input
+                type="text"
+                placeholder="Activity name"
+                class="w-full rounded border px-4 text-xl"
+            >
+            <BaseButton>
+                <PlusIcon class="h-8" />
+            </BaseButton>
+        </form>
     </div>
 </template>
 
