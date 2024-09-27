6c9dec57c97255f973905fd0849327eae83c7d82
kovaldos <ya.kovalev@ya-kovalev.ru>
2024-09-27 12:20:49 +0300

    added activity adding form component

---
+++ src/components/TheActivityForm.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { PlusIcon } from '@heroicons/vue/24/outline/index.js'
+import BaseButton from './BaseButton.vue'
+import { isActivityValid } from '../validators'
+
+const emit = defineEmits({
+    submit: isActivityValid,
+})
+
+let activity = ''
+
+function submit() {
+    emit('submit', activity)
+}
+</script>
+
+<template>
+    <form
+        class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4"
+        @submit.prevent="submit"
+    >
+        <input
+            type="text"
+            placeholder="Activity name"
+            :value="activity"
+            class="w-full rounded border px-4 text-xl"
+            @input="activity = $event.target.value"
+        >
+        <BaseButton>
+            <PlusIcon class="h-8" />
+        </BaseButton>
+    </form>
+</template>
+
+<style scoped></style>
